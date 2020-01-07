be353d166a989a44190aa4f51a63ca65e10a8d58
mshaileshr@gmail.com <Yes@383840>
2020-01-07 17:29:55 +0530

    code structured

--- lib/postpage/postdetail_page.dart
+++ lib/postpage/postdetail_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:healthnest/widgets/drop_down.dart';
+import 'package:healthnest/postpage/widgets.dart';
 import 'package:share/share.dart';
 
 class PostPageDetails extends StatefulWidget {
