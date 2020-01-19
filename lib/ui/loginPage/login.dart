7b8b9b50a7d7b4826f22c23db81fdacae1f58e4f
mshaileshr@gmail.com <Yes@383840>
2020-01-19 13:50:44 +0530

    file structured

--- lib/ui/loginPage/login.dart
+++ lib/ui/loginPage/login.dart
@@ -30,101 +30,41 @@ class _HealthNestLoginState extends State<HealthNestLogin> {
     );
   }
 
+
   Widget formUI() {
     return GestureDetector(
       onTap: () => FocusScope.of(context).requestFocus(new FocusNode()),
       child: Scaffold(
         body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.only(
-                top: 150.0, left: 16, right: 16, bottom: 16),
+          child: Container(
+            padding: const EdgeInsets.only(top: 100.0, left: 16, right: 16, bottom: 16),
             child: SafeArea(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.end,
                 children: <Widget>[
-                  Align(
-                      alignment: Alignment.topLeft,
-                      child: Image.asset('assets/img/doctor.png',
-                          width: 80, height: 80, color: Colors.teal)),
+                  appIcon(),
                   SizedBox(height: 20),
                   Container(
                     //alignment: Alignment.bottomLeft,
 
-                    child: const Text(
-                      'Welcome to\nHealthNest',
+                    child: const Text('Welcome to\nHealthNest',
                       style: TextStyle(
                           fontSize: 30,
                           fontStyle: FontStyle.normal,
                           fontWeight: FontWeight.bold),
                     ),
                   ),
-                  SizedBox(height: 40),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
-                      Expanded(
-                        flex: 1,
-                        child: CountryPicker(
-                          showDialingCode: true,
-                          showName: false,
-                          showCurrencyISO: false,
-                          onChanged: (Country country) {
-                            setState(() {
-                              _selectedCountry = country;
-                            });
-                          },
-                          selectedCountry: _selectedCountry,
-                        ),
-                      ),
-                      SizedBox(width: 20),
-                      Expanded(
-                        flex: 2,
-                        child: TextFormField(
-                          maxLength: 10,
-                          keyboardType: TextInputType.number,
-                          decoration: InputDecoration(
-                              labelText: 'Enter phone number',
-                              suffixIcon: Icon(CupertinoIcons.phone)),
-                          validator: _validateMobile,
-                          controller: controller,
-                          onSaved: (String phoneNumber) {
-                            consumerMobileNumber = phoneNumber;
-                          },
-                        ),
-                      )
-                    ],
-                  ),
-                  SizedBox(height: 50),
-                  Container(
-                      width: 350,
-                      child: Text(
-                        'We never compromise on security!\nHelp us create a safe place by providing your mobile number to maintain authenticity',
-                        style: TextStyle(
-                            fontSize: 15,
-                            fontWeight: FontWeight.normal,
-                            color: Colors.grey),
-                      )),
-                  SizedBox(height: 40),
-                  Container(
-                    width: double.infinity,
-                    height: 40,
-                    child: RaisedButton(
-                      //onPressed: _isButtonDisabled ? null : () {_sendToServer();},
-                      onPressed: _sendToServer,
-                      child: Text('Send OTP',
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.bold)),
-                      //color: Theme.of(context).accentColor,
-                      elevation: 5,
-                    ),
-                  ),
+                  SizedBox(height: 20),
+                  editTextWithCountryCode(),
+                  SizedBox(height: 30),
+                  textDescription(),
+                  SizedBox(height: 30),
+                  sendOTPButtonWidget(),
                 ],
               ),
             ),
           ),
-        ),
+      ),
       ),
     );
   }
@@ -157,5 +97,75 @@ class _HealthNestLoginState extends State<HealthNestLogin> {
         _validate = true;
       });
     }
+  }
+
+  Widget sendOTPButtonWidget() {
+    return Container(
+      width: double.infinity,
+      height: 40,
+      child: RaisedButton(
+        //onPressed: _isButtonDisabled ? null : () {_sendToServer();},
+        onPressed: _sendToServer,
+        child: Text('Send OTP',
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+        //color: Theme.of(context).accentColor,
+        elevation: 5,
+      ),
+    );
+  }
+
+  Widget textDescription() {
+    return Container(
+        child: Text(
+      'We never compromise on security!\nHelp us create a safe place by providing your mobile number to maintain authenticity',
+      style: TextStyle(
+          fontSize: 15, fontWeight: FontWeight.normal, color: Colors.grey),
+    ));
+  }
+
+  Widget editTextWithCountryCode() {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Expanded(
+          flex: 1,
+          child: CountryPicker(
+            showDialingCode: true,
+            showName: false,
+            showCurrencyISO: false,
+            onChanged: (Country country) {
+              setState(() {
+                _selectedCountry = country;
+              });
+            },
+            selectedCountry: _selectedCountry,
+          ),
+        ),
+        SizedBox(width: 20),
+        Expanded(
+          flex: 2,
+          child: TextFormField(
+            maxLength: 10,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+                labelText: 'Enter phone number',
+                suffixIcon: Icon(CupertinoIcons.phone)),
+            validator: _validateMobile,
+            controller: controller,
+            onSaved: (String phoneNumber) {
+              consumerMobileNumber = phoneNumber;
+            },
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget appIcon() {
+    return Align(
+        alignment: Alignment.topLeft,
+        child: Image.asset('assets/img/doctor.png',
+            width: 80, height: 80, color: Colors.teal));
   }
 }
